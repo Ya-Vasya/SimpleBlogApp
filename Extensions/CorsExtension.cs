f22d1ba674abdf7c79cd04e2808de3e8aebc15c6
ya-vasya <vasyl.stashchuk.pz.2018@lpnu.ua>
2021-09-04 20:14:08 +0300

    Setup project, added ef, added extension methods, connection strings etc.

---
+++ Extensions/CorsExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleBlog.API.Extensions
+{
+    public static class CorsExtension
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+    }
+}
